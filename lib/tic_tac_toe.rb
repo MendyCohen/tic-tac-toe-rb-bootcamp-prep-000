ae5fbf1517d73280c6f70ba015e413add8fd3631
Mendy <mendybeltcohen@gmail.com>
2018-06-20 20:33:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -123,7 +123,7 @@ def won?(board)
         play(board)
       elsif won?(board)
         puts "Congratulations #{board[won?(board)[0]]}!"
-        elsif draw?(board)
+      elsif draw?(board)
         puts "Cat's Game!"
       end
     end
