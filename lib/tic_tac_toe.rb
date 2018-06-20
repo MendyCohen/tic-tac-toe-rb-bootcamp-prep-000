bc4274872a11bb3a90f13f60e3f9125da492c780
Mendy <mendybeltcohen@gmail.com>
2018-06-20 21:21:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
- board = [" "," "," "," "," "," "," "," "," "]
+ #board = [" "," "," "," "," "," "," "," "," "]
 
 def input_to_index(user_input)
   user_input.to_i - 1
